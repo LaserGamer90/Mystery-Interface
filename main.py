425727467dcb0661ea60c9fcf453e0466efdbdc6
RunawayReptile <87612791+RunawayReptile@users.noreply.github.com>
2023-07-24 16:37:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from ursina import *
 from ursina.shaders import *
 from ursina.prefabs.first_person_controller import FirstPersonController
+import tkinter
+#
+#
 Game = Ursina()
 window.borderless = False
 window.cog_button.disable()
 window.fps_counter.disable()
 window.exit_button.disable()
 Sky()
-Floor = Entity(model="plane", collider="mesh", texture="white_cube",scale=(50,50))
+def input():
+    if key == "tab":
+        EditorCamera()
+    if key == "escape":
+        mouse.locked = not mouse.locked
+        
+Floor = Entity(model="plane", collider="mesh", texture="white_cube",scale=(50,1,50),texture_scale=(50,50))
 Player = FirstPersonController()
 Game.run()
